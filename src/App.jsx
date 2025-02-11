1e9269ddf0c838d6d8dd2033d2eef0fc9c85ee92
nimra-ali <nimrakhan098ali@gmail.com>
2025-02-11 17:51:56 +0500

    solve build errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Chat from "./components/chat/Chat";
-import Detail from "./components/detail/detail"
-import List from "./components/list/list"
+import Detail from "./components/detail/Detail"
+import List from "./components/list/List"
 import Login from "./components/Login/Login";
 import Notification from "./components/Notification/Notification";
 
