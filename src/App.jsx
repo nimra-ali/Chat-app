30e1ee0d74f562c17f56fb2492f22884813607c2
nimra-ali <nimrakhan098ali@gmail.com>
2025-02-11 18:06:35 +0500

    solve warnings

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Notification from "./components/Notification/Notification";
 
 const App = () => {
 
-  const user = false;
+  const user = true;
 
   return (
     <div className='container'>
